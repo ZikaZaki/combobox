40ea4936fad60186840b40844f283a1a1ecd9ccf
zikazaki <17160061@su.edu.ye>
2023-01-23 03:14:15 +0300

    Add tailwind custom-combobox

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  plugins: [require("@tailwindcss/forms")],
+  content: [
+    "./app/components/**/*.html.erb",
+    "./app/components/**/*.rb",
+    "./app/views/**/*.html.erb",
+    "./app/helpers/**/*.rb",
+    "./app/assets/stylesheets/**/*.css",
+    "./app/javascript/**/*.js",
+  ]
+}
